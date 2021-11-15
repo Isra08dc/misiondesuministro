5e7ee30523328ee49699c08224d6a18416c3481c
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2021-11-15 17:57:46 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -76,7 +76,7 @@ function draw() {
 
   if(keyCode === LEFT_ARROW) {
 	  helicopterSprite.x=helicopterSprite.x-20;
-	  Matter.Body.translate(packageBody,{x:-20,y:0});
+	  Matter.Body.translate(packageBody,{x:+20,y:0});
   }
 	  if(keyCode === RIGHT_ARROW) {
 		helicopterSprite.x=helicopterSprite.x+20;
